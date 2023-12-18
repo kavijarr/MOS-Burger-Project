9b260178880f332c34fa9b90de86faba2638dc70
kavijarr <kavijakumuditha12@gmail.com>
2023-12-18 18:21:36 +0530

    all products

--- assets/js/productsPage.js
+++ assets/js/productsPage.js
@@ -186,8 +186,203 @@ let submarines=[{
 },
 ];
 
+let fries = [{
+        "name" : "Steak Fries",
+        "id" : "B1025",
+        "size" : "Large",
+        "discount" : "0%",
+        "price" : 1200,
+        "img" : "./assets/img/fries/steak fries.jpg"
+},
+{
+    "name" : "Steak Fries",
+    "id" : "B1026",
+    "size" : "Medium",
+    "discount" : "0%",
+    "price" : 600,
+    "img" : "./assets/img/fries/steak fries.jpg"
+},
+{
+    "name" : "French Fries",
+    "id" : "B1027",
+    "size" : "Large",
+    "discount" : "0%",
+    "price" : 800,
+    "img" : "./assets/img/fries/french fries.jpg"
+},
+{
+    "name" : "French Fries",
+    "id" : "B1028",
+    "size" : "Medium",
+    "discount" : "0%",
+    "price" : 650,
+    "img" : "./assets/img/fries/french fries.jpg"
+},
+{
+    "name" : "French Fries",
+    "id" : "B1029",
+    "size" : "Small",
+    "discount" : "0%",
+    "price" : 450,
+    "img" : "./assets/img/fries/french fries.jpg"
+},
+{
+    "name" : "Sweet Potato Fries",
+    "id" : "B1030",
+    "size" : "Large",
+    "discount" : "0%",
+    "price" : 600,
+    "img" : "./assets/img/fries/sweet potato fries.jpg"
+},
+];
+
+let pastas = [{
+        "name" : "Chicken n Cheese Pasta",
+        "id" : "B1031",
+        "size" : "Regular",
+        "discount" : "15%",
+        "price" : 1600,
+        "img" : "./assets/img/pasta/chicken cheese pasta.jpg"
+    },
+    {
+        "name" : "Chicken Penne Pasta",
+        "id" : "B1032",
+        "size" : "Regular",
+        "discount" : "0%",
+        "price" : 1700,
+        "img" : "./assets/img/pasta/chicken penne pasta.jpg"
+    },
+    {
+        "name" : "Ground Turkey Pasta Bake ",
+        "id" : "B1033",
+        "size" : "Regular",
+        "discount" : "10%",
+        "price" : 2900,
+        "img" : "./assets/img/pasta/ground turkey pasta bake.jpg"
+    },
+    {
+        "name" : "Creamy Shrimp Pasta",
+        "id" : "B1034",
+        "size" : "Regular",
+        "discount" : "0%",
+        "price" : 2000,
+        "img" : "./assets/img/pasta/creamy shrimp pasta.jpg"
+    },
+    {
+        "name" : "Lemon Butter Pasta",
+        "id" : "B1035",
+        "size" : "Regular",
+        "discount" : "0%",
+        "price" : 1950,
+        "img" : "./assets/img/pasta/lemon butter pasta.jpg"
+    },
+    {
+        "name" : "Tagliatelle Pasta",
+        "id" : "B1036",
+        "size" : "Regular",
+        "discount" : "1%",
+        "price" : 2400,
+        "img" : "./assets/img/pasta/tagliatelle pasta.jpg"
+    },
+    {
+        "name" : "Baked Ravioli",
+        "id" : "B1037",
+        "size" : "Regular",
+        "discount" : "1%",
+        "price" : 2000,
+        "img" : "./assets/img/pasta/baked ravioli.jpg"
+    },
+];
+
+let chicken = [{
+    "name" : "Fried Chicken",
+    "id" : "B1038",
+    "size" : "Small",
+    "discount" : "0%",
+    "price" : 1200,
+    "img" : "./assets/img/chicken/fried chicken.jpg"
+},
+{
+    "name" : "Fried Chicken",
+    "id" : "B1039",
+    "size" : "Regular",
+    "discount" : "10%",
+    "price" : 2300,
+    "img" : "./assets/img/chicken/fried chicken.jpg"
+},
+{
+    "name" : "Fried Chicken",
+    "id" : "B1040",
+    "size" : "Large",
+    "discount" : "5%",
+    "price" : 3100,
+    "img" : "./assets/img/chicken/fried chicken.jpg"
+},
+{
+    "name" : "Hot Wings",
+    "id" : "B1041",
+    "size" : "Large",
+    "discount" : "0%",
+    "price" : 2400,
+    "img" : "./assets/img/chicken/hot wings.jpg"
+},
+{
+    "name" : "Devilled Chicken",
+    "id" : "B1042",
+    "size" : "Large",
+    "discount" : "0%",
+    "price" : 900,
+    "img" : "./assets/img/chicken/devilled chicken.jpg"
+},
+{
+    "name" : "BBQ Chicken",
+    "id" : "B1043",
+    "size" : "Regular",
+    "discount" : "0%",
+    "price" : 2100,
+    "img" : "./assets/img/chicken/bbq chicken.jpg"
+},
+];
+
+let beverages = [{
+    "name" : "Pepsi",
+    "id" : "B1044",
+    "size" : "330ml",
+    "discount" : "5%",
+    "price" : 990,
+    "img" : "./assets/img/beverages/pepsi.png"
+},
+{
+    "name" : "Coca-Cola",
+    "id" : "B1045",
+    "size" : "330ml",
+    "discount" : "0%",
+    "price" : 1230,
+    "img" : "./assets/img/beverages/coca cola.jpg"
+},
+{
+    "name" : "Sprite",
+    "id" : "B1046",
+    "size" : "330ml",
+    "discount" : "3%",
+    "price" : 1500,
+    "img" : "./assets/img/beverages/sprite.png"
+},
+{
+    "name" : "Mirinda",
+    "id" : "B1047",
+    "size" : "330ml",
+    "discount" : "7%",
+    "price" : 1500,
+    "img" : "./assets/img/beverages/mirinda.png"
+},
+];
 let burgersList = document.querySelector('.burgers');
 let submarineList = document.querySelector('.submarines');
+let friesList = document.querySelector('.fries');
+let pastaList = document.querySelector('.pasta');
+let chickenList = document.querySelector('.chicken');
+let beveragesList = document.querySelector('.beverages');
 
 function loadBurgers(){
     burgers.forEach((value,key)=>{
@@ -221,6 +416,70 @@ function loadSubmarines(){
     })
 }
 
+function loadFries(){
+    fries.forEach((value,key)=>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('col-lg-6');
+        newDiv.innerHTML = `
+        <div class="products rounded-4">
+        <img src="${value.img}" id="productImg" class="product-img" alt="">
+        <label for="productImg" class="ms-4 text-center">${value.name} (${value.size}) (${value.id}) <br/> ${value.discount} Discount </label><br>
+        <p class="text-center price">Rs. ${value.price}.00</p>
+        <div class="text-center"><button class="btn btnAddtoCard">Add to Cart</button></div>
+      </div> 
+      `;
+        friesList.appendChild(newDiv);
+    })
+}
+
+function loadpasta(){
+    pastas.forEach((value,key)=>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('col-lg-6');
+        newDiv.innerHTML = `
+        <div class="products rounded-4">
+        <img src="${value.img}" id="productImg" class="product-img" alt="">
+        <label for="productImg" class="ms-4 text-center">${value.name} (${value.size}) (${value.id}) <br/> ${value.discount} Discount </label><br>
+        <p class="text-center price">Rs. ${value.price}.00</p>
+        <div class="text-center"><button class="btn btnAddtoCard">Add to Cart</button></div>
+      </div> 
+      `;
+        pastaList.appendChild(newDiv);
+    })
+}
+
+function loadChicken(){
+    chicken.forEach((value,key)=>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('col-lg-6');
+        newDiv.innerHTML = `
+        <div class="products rounded-4">
+        <img src="${value.img}" id="productImg" class="product-img" alt="">
+        <label for="productImg" class="ms-4 text-center">${value.name} (${value.size}) (${value.id}) <br/> ${value.discount} Discount </label><br>
+        <p class="text-center price">Rs. ${value.price}.00</p>
+        <div class="text-center"><button class="btn btnAddtoCard">Add to Cart</button></div>
+      </div> 
+      `;
+        chickenList.appendChild(newDiv);
+    })
+}
+
+function loadbeverages(){
+    beverages.forEach((value,key)=>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('col-lg-6');
+        newDiv.innerHTML = `
+        <div class="products rounded-4">
+        <img src="${value.img}" id="productImg" class="product-img" alt="">
+        <label for="productImg" class="ms-4 text-center">${value.name} (${value.size}) (${value.id}) <br/> ${value.discount} Discount </label><br>
+        <p class="text-center price">Rs. ${value.price}.00</p>
+        <div class="text-center"><button class="btn btnAddtoCard">Add to Cart</button></div>
+      </div> 
+      `;
+        beveragesList.appendChild(newDiv);
+    })
+}
+
 function scrollSubmarine(){
     const submarine = document.getElementById("sub");
     submarine.scrollIntoView();
@@ -233,5 +492,9 @@ function scrollBurger(){
 
 loadBurgers();
 loadSubmarines();
+loadFries();
+loadpasta();
+loadChicken();
+loadbeverages();
 
 
